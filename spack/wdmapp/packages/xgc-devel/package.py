28470f0fc9317c58491633b855a4b5bb41b8b7d9
Cameron Smith <smithc11@rpi.edu>
2020-04-27 22:08:05 -0400

    xgc-devel: rpi branch
    use a tagged version of kokkos

---
+++ spack/wdmapp/packages/xgc-devel/package.py
@@ -0,0 +1,39 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+class XgcDevel(CMakePackage):
+    """XGC-Devel used for coupling with GENE-CUTH via the coupler"""
+
+    homepage = "https://hbps.pppl.gov/computing/xgc-1"
+    # FIXME, there is no tarball, but it still needs a URL, so it's fake
+    url      = "https://github.com/wdmapp/xgc1.tar.gz"
+    git      = "git@github.com:wdmapp/XGC-Devel.git"
+
+    maintainers = ['germasch', 'bd4', 'cwsmith', 'Damilare06']
+
+    version('rpi', branch='rpi')
+
+    variant('coupling_core_edge_gene', default=False,
+            description='Enable XGC_USE_GENE_COUPLING')
+
+    depends_on('petsc@3.7.0:3.7.999 ~complex +double')
+    depends_on('pkgconfig')
+    depends_on('adios +fortran')
+    depends_on('adios2 +fortran')
+    depends_on('fftw@3.3.8:')
+    depends_on('cabana@develop +openmp', when='@rpi')
+
+    def cmake_args(self):
+        spec = self.spec
+        args = []
+        args += ['-DBUILD_TESTING=OFF']
+        args += ['-DXGC_USE_ADIOS1=ON']
+        args += ['-DXGC_USE_ADIOS2=ON']
+        args += ['-DXGC_USE_CABANA=ON']
+        args += ['-DXGC_USE_GENE_COUPLING={}'.format('ON' if '+coupling_core_edge_gene' in spec else 'OFF')]
+        return args
+
